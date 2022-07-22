1cddb2931dd85b6186c9e74339c341a926d4fc11
q10rcode <90580184+q10rcode@users.noreply.github.com>
2022-07-22 16:05:01 -0700

    Commiting ReadMe along with some changes

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# codepath-prework
-Project for week 0 codepath
+## CodePath Intro to Mobile App Dev Week 0
+
+### App Description
+This app opens up with a message stating "Hello from Quinten." There are four buttons available for users to manipulate this message. They can change the color to white using the "Change text color" button. The "Change view button" makes the background teal. Users are able to change the message displayed by entering text and pressing "Change text string." Users can also select random to have random colors for the text and background. Tapping the background will restore the original settings.
+
+### App Walk-though
+
+
+<img src="http://g.recordit.co/kNfybkjDoO.gif" width=200><br>
+
+
+### Required User Stories
+- [x] 1. User sees custom text in a label - Hello from {name}!
+- [x] 2. User see's custom background color.
+- [x] 3. User can tap a button to change the text color of the label.
+
+### Optional User Stories
+- [x] 1. User can tap a button to change the color of the background view.
+- [x] 2. User can tap a button to change the text string of the label - Goodbye 👋.
+- [x] 3. User can tap on the background view to reset all views to default settings.
+- [x] 4. User can update the label text with custom text entered into the text field.
+   - [x] a. User can enter text into a text field using the keyboard.
+   - [x] b. User can tap the "Change text string" button to update the label with the text from the text field.
+   - [x] c. If the text field is empty, update label with default text string.
+   - [x] d. The keyboard is dismissed after the button has been tapped.
--- app/src/main/java/com/example/mycodepathproject/MainActivity.kt
+++ app/src/main/java/com/example/mycodepathproject/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mycodepathproject
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -23,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         //Shows the layout file: activity_main
         setContentView(R.layout.activity_main)
+
+        val origColor = findViewById<TextView>(R.id.textView).textColors
 
         //User can press button to change text color
         //1. Add a button to layout
@@ -44,8 +48,8 @@ class MainActivity : AppCompatActivity() {
             findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(resources.getColor(R.color.teal_200))
         }
 
-        //Create logic for textString
-        findViewById<Button>(R.id.textString).setOnClickListener{
+        //Create logic for textString (Changing the string on screen)
+        findViewById<Button>(R.id.textString).setOnClickListener{ v->
             //Check if button works
             Log.i("Quinten", "Change text string pressed")
 
@@ -55,10 +59,39 @@ class MainActivity : AppCompatActivity() {
             message = findViewById<EditText>(R.id.userString).text.toString()
             //Place string in textView
             findViewById<TextView>(R.id.textView).text = message
+
+            //Print message if userString is blank
+            if (message == ""){
+                val defMessage = "Goodbye!"
+                findViewById<TextView>(R.id.textView).text = defMessage
+            }
+            //Hide keyboard once button is pressed
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(v.windowToken, 0)
+
+        }
+
+        //Reset everything when the background is tapped
+        findViewById<ConstraintLayout>(R.id.backgroundView).setOnClickListener {
+            //Check if background is tapped
+            Log.i("Quinten", "Background pressed")
+
+            val origMessage = "Hello from Quinten"
+            //Revert textView
+            findViewById<TextView>(R.id.textView).text = origMessage
+
+            //Turn background back to original color
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(resources.getColor(R.color.purple_200))
+
+            //Turn the text back to original color
+            findViewById<TextView>(R.id.textView).setTextColor(origColor)
         }
 
         //Create logic for randButton
         findViewById<Button>(R.id.randButton).setOnClickListener{
+            //Check is the randButton works
+            Log.i("Quinten", "Random button pressed")
+
             //First get random numbers for rgb
             var a = getRandomColor() // a, b, and c will hold random int for rgb colors
             var b = getRandomColor()
